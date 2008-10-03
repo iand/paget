c8aa5e5691d04d75d318cb7fd367faf0504ebece
me@iandavis.com <me@iandavis.com@1cea924b-614f-0410-af49-5dc9638ebc3f>
2008-10-03 00:11:19 +0000

    FileGenerator can now read turtle and json files as well as rdf/xml

--- paget_filegenerator.class.php
+++ paget_filegenerator.class.php
@@ -20,15 +20,30 @@ Suite 330, Boston, MA 02111-1307 USA
 class PAGET_FileGenerator {
   var $_config = array();
   var $_filename;
+  var $_type;
   
   function __construct(&$config, &$params) {
     $this->_config = $config;  
     $this->_filename = $params['file'];
+    if (isset($params['type'])) {
+      $this->_type = $params['type'];
+    }
+    else {
+      $this->_type = 'rdfxml';
+    }
   }
   
   function process(&$desc, $request) {
     $data = file_get_contents($this->_filename);
-    $desc->add_rdfxml($data);
+    if ( $this->_type === 'rdfxml' ) {
+      $desc->add_rdfxml($data);
+    }
+    else if ( $this->_type === 'turtle' ) {
+      $desc->add_turtle($data);
+    }
+    else if ( $this->_type === 'json' ) {
+      $desc->add_json($data);
+    }
   }
 
 }

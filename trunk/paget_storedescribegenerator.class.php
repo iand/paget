93c33177d28364a2c3cc438d20e0e2dd8adb36bd
me@iandavis.com <me@iandavis.com@1cea924b-614f-0410-af49-5dc9638ebc3f>
2009-03-25 22:50:17 +0000

    StoreDescribeGenerator takes a description type parameter on constructor

--- trunk/paget_storedescribegenerator.class.php
+++ trunk/paget_storedescribegenerator.class.php
@@ -3,14 +3,16 @@ require_once MORIARTY_DIR . 'store.class.php';
 
 class PAGET_StoreDescribeGenerator {
   var $_store_uri;
+  var $_type;
   
-  function __construct($store_uri) {
+  function __construct($store_uri, $type = 'cbd') {
     $this->_store_uri = $store_uri;
+    $this->_type = $type;
   }
   
   function add_triples($resource_uri, &$desc) {
     $store = new Store($this->_store_uri);
-    $response = $store->describe($resource_uri);
+    $response = $store->describe($resource_uri, $this->_type);
     if ($response->is_success()) {
       $desc->add_rdfxml($response->body);
     }

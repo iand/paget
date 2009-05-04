6d5ef89d3a5295834b1eea61960620b8d2ef5cb7
me@iandavis.com <me@iandavis.com@1cea924b-614f-0410-af49-5dc9638ebc3f>
2009-05-04 19:32:00 +0000

    removed subtitle from get_label added dct:hasVersion to simple property labeller

--- paget_resourcedescription.class.php
+++ paget_resourcedescription.class.php
@@ -128,15 +128,6 @@ class PAGET_ResourceDescription extends SimpleGraph {
     if ( strlen($label) == 0) {
       $label = $this->get_first_literal($resource_uri,RDF_VALUE, '', 'en');
     }
-    $subtitle = $this->get_first_literal($resource_uri,'http://open.vocab.org/terms/subtitle', '', 'en');  
-    if ( strlen($subtitle) > 0) {
-      if ( strlen($label) == 0) {
-        $label = $subtitle; 
-      }
-      else {
-        $label .= ': ' . $subtitle;        
-      }
-    } 
     if ( strlen($label) == 0) {
       $label = $resource_uri;
     }  
